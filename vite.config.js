8d71449ed1ce308f4733e88b3be700360efede83
Iftikhar Ahmed <ifti3061@gmail.com>
2026-03-05 18:41:43 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/MIssion-A2/',
+  base: '/Mission-A2/',
   plugins: [
     react(),
     tailwindcss(),
